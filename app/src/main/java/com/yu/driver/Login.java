fd9aa45bd2953537695c4795c73e100c8d49eb34
Yu <nekochan0429@gmail.com>
2020-05-13 13:26:08 +0800

    新增偏好設定

--- app/src/main/java/com/yu/driver/Login.java
+++ app/src/main/java/com/yu/driver/Login.java
@@ -2,6 +2,7 @@ package com.yu.driver;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
@@ -19,10 +20,13 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.lu.driver.Common;
 import com.lu.driver.CommonTask;
 import com.lu.driver.R;
+
+import static android.content.Context.MODE_PRIVATE;
 
 
 /**
@@ -33,10 +37,13 @@ public class Login extends Fragment {
     private EditText etEmail, etPassword;
     private TextView tvForgetPassword, tvSignUp;
     private Button btLogin;
-    private CommonTask commonTask;
+    private CommonTask commonTask, getUserInfoTask;
     private final static String TAG = "Login";
     private TextInputLayout tilEmail, tilPassword;
-
+    private static int driver_id = 0;
+    private static String driver_name = null;
+    private SharedPreferences preferences;
+    private final static String PREFERENCES_NAME = "preferences";
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -63,6 +70,8 @@ public class Login extends Fragment {
         tilPassword = view.findViewById(R.id.tilPassword);
         tvForgetPassword = view.findViewById(R.id.tvForgetPassword);
         tvSignUp = view.findViewById(R.id.tvSignUp);
+
+        preferences = activity.getSharedPreferences(PREFERENCES_NAME, MODE_PRIVATE);   //取得偏好設定檔
 
 
         tvForgetPassword.setOnClickListener(new View.OnClickListener() {
@@ -97,6 +106,7 @@ public class Login extends Fragment {
                     jsonObject.addProperty("password", Password);
                     commonTask = new CommonTask(url, jsonObject.toString());//給伺服器位置，為傳送資料做準備
                     int loginResult = 0;
+
                     try {
                         String Login = commonTask.execute().get();//取得資料，啟動，呼叫execute，doinbackground會執行，呼叫get等開啟的新執行緒doinbackground，抓完資料回傳回來
 //                        JsonObject jObject = new Gson().fromJson(Login, JsonObject.class);// Json 字串，直接轉成該類別的物件
@@ -110,6 +120,31 @@ public class Login extends Fragment {
                     } else if (loginResult == 2 || Password.isEmpty()) {
                         tilPassword.setError("密碼輸入錯誤");
                     } else if (loginResult == 1) {
+//                      JsonObject jsonFindId = new JsonObject();   //建一個物件
+//                      jsonFindId.addProperty("action", "findId");
+//                      jsonFindId.addProperty("email", Email);//幫key加屬性，送到伺服器
+                        JsonObject jsonGetUserInfo = new JsonObject();   //建一個物件
+                        jsonGetUserInfo.addProperty("action", "getUserInfo");
+                        jsonGetUserInfo.addProperty("email", Email);//幫key加屬性，送到伺服器
+                        String jsonOut = jsonGetUserInfo.toString();
+                        getUserInfoTask = new CommonTask(url, jsonOut);
+
+                        try {
+//                            CommonTask findIDTask = new CommonTask(url, jsonFindId.toString());
+//                            String inputDriverId = findIDTask.execute().get();
+//                            driver_id = Integer.valueOf(inputDriverId);
+                            String jsonIn = getUserInfoTask.execute().get();
+                            JsonObject jsonResult = new Gson().fromJson(jsonIn, JsonObject.class);
+                            driver_id = jsonResult.get("driver_id").getAsInt();
+                            driver_name = jsonResult.get("driver_name").getAsString();
+//                            driver_id = Integer.valueOf(inputDriverId);
+//                            driver_name = String.valueOf(inputDriverName);
+
+
+                            savePreferences();
+                        } catch (Exception e) {
+                            Log.e(TAG, "getString" + e.toString());
+                        }
                         Common.showToast(activity, "登入成功");
                         Navigation.findNavController(v).navigate(R.id.action_login_to_mainFragment);
                     } else {
@@ -121,6 +156,21 @@ public class Login extends Fragment {
         });
 
     }
+
+
+    private void savePreferences() {
+        preferences.edit()   //開啟編輯狀態，編輯的東西暫存到記憶體裡
+                .putInt("driver_id", driver_id)
+                .putString("driver_name", driver_name)
+                .apply();   //真的把記憶體的東西存到檔案裡
+        Common.showToast(activity, "歡迎回來"+driver_name);
+    }
+
+//    private void loadPreferences() {
+//        preferences = activity.getSharedPreferences(PREFERENCES_NAME, MODE_PRIVATE);   //取得偏好設定檔，偏好設定檔可以讀多個
+//        loadPreferences();
+//        int driver_id = preferences.getInt("driver_id", driver_id);
+//    }
 
     //檢察網路狀態
     private boolean networkConnected() {
